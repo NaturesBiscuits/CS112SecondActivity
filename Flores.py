f2545d55e170c58da523fdfe841eb27059026811
NaturesBiscuits <135051519+NaturesBiscuits@users.noreply.github.com>
2023-09-20 22:17:17 +0800

    Flores.py

--- Flores.py
+++ Flores.py
@@ -3,10 +3,10 @@ id = input("ID number: ")
 course = input("Course: ")
 section = input("Section: ")
 
-first = float(input("Enter 1st quarter: "))
-second = float(input("Enter 2nd quarter: "))
-third = float(input("Enter 3rd quarter: "))
-fourth = float(input("Enter 4th quarter: "))
+first = float(input("Enter 1st quarter grade: "))
+second = float(input("Enter 2nd quarter grade: "))
+third = float(input("Enter 3rd quarter grade: "))
+fourth = float(input("Enter 4th quarter grade: "))
 
 average = (first + second + third + fourth) / 4
 
